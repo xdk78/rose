8dd1d03b6a9bcf3492a281e13355c0f65d1fbbb5
xdk78 <xdk78@interia.pl>
2018-07-29 20:24:08 +0200

    feat: prepare file reading from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        3 => {
+            let cmd = &args[1];
+            let file = &args[2];
+            // parse the command
+            match &cmd[..] {
+                "run" => run_file(file),
+                _ => {
+                    eprintln!("error: invalid command");
+                }
+            }
+        }
+        _ => {
+            eprintln!("error: no arguments passed!");
+        }
+    }
+}
+
+fn run_file(filename: &str) {
+    let mut file = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("{}", contents);
 }
